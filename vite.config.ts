f7689fbb0738d9f4221bdf2dd0310f15157c2566
Dharav <dharav9shah@gmail.com>
2025-06-04 15:35:00 -0400

    build push

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: 'https://dharavshahcodes.co/',
   plugins: [react()],
   resolve: {
     alias: {
